a603134e11ce353f297f1b3613eb57bde073faea
Henrique <hss@outlook.com.br>
2025-05-12 16:00:30 -0300

    Adjusting docker configuration (#6)

--- src/main/java/com/hss/cryptohash/commons/dto/EncryptionResponseDTO.java
+++ src/main/java/com/hss/cryptohash/commons/dto/EncryptionResponseDTO.java
@@ -1,6 +1,9 @@
 package com.hss.cryptohash.commons.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.io.Serializable;
 
+@RegisterForReflection
 public record EncryptionResponseDTO(String passwordEncrypted) implements Serializable {
 }
--- src/main/java/com/hss/cryptohash/commons/dto/GeneralResponseDTO.java
+++ src/main/java/com/hss/cryptohash/commons/dto/GeneralResponseDTO.java
@@ -1,5 +1,8 @@
 package com.hss.cryptohash.commons.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record GeneralResponseDTO<T>(T data) {
 
 }
